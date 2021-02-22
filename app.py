f5c7a77c9e7cf22a7d8af92ddeaaafd469bcf3b5
Claudio-C-Santos <cula.santos@gmail.com>
2021-02-22 17:33:08 +0000

    <build> Register page

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from alpha_vantage.timeseries import TimeSeries
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -26,11 +27,38 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists
+        existing_user = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successfull")
+
+    return render_template("register.html")
+
+
 @app.route("/get_transactions")
 def get_transactions():
     transactions = mongo.db.transactions.find()
     stock = app_alpha.get_daily_adjusted("AAPL")
-    return render_template("transactions.html", transactions=transactions, stock=stock)
+    return render_template(
+                            "transactions.html",
+                            transactions=transactions, stock=stock)
 
 
 if __name__ ==   "__main__":
