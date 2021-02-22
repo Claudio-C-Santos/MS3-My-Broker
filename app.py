d0f1b2873acb6ead7c0ace375d0345e5a1781f7c
Claudio-C-Santos <cula.santos@gmail.com>
2021-02-22 12:42:55 +0000

    <build> index page

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from flask import (
     session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from alpha_vantage.timeseries import TimeSeries
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app_alpha = TimeSeries("Q6C1O1J04IYQXEUN")
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -19,10 +22,15 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/get_transactions")
 def get_transactions():
     transactions = mongo.db.transactions.find()
-    return render_template("transactions.html", transactions=transactions)
+    stock = app_alpha.get_daily_adjusted("AAPL")
+    return render_template("transactions.html", transactions=transactions, stock=stock)
 
 
 if __name__ ==   "__main__":
