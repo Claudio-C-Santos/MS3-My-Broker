ca872fc66fffbd805a21e8ca1ff4b9de8a3c56fd
Claudio-C-Santos <cula.santos@gmail.com>
2021-03-10 13:37:37 +0000

    <build>Js to change funds color to red when negative

---
+++ static/js/script-3.js
@@ -0,0 +1,19 @@
+let funds_el_profile = document.querySelector("#funds_available_profile")
+let funds_profile = document.querySelector("#funds_available_profile").innerHTML
+
+int_funds_profile = parseFloat(funds_profile.replace(",", ""))
+
+if (int_funds_profile < 0) {
+    funds_el_profile.removeAttribute("class", "text-gray-800")
+    funds_el_profile.setAttribute("class", "negative_wallet_profile");
+}
+
+let funds_el_bar = document.querySelector("#funds_available_bar")
+let funds_bar = document.querySelector("#funds_available_bar").innerHTML
+
+int_funds_bar = parseFloat(funds_bar.replace(",", ""))
+
+if (int_funds_bar < 0) {
+    funds_el_bar.removeAttribute("class", "text-gray-800")
+    funds_el_bar.setAttribute("class", "negative_wallet_bar");
+}
--- static/js/script.js
+++ static/js/script.js
@@ -5,9 +5,8 @@ let total_amount = document.querySelector("#money_amount");
 // For the purchase page to generate the result autmatically
 input_amount.addEventListener("input", function() {
     let price = input_amount.value * stock_quote.value
-    total_amount.value = price.toFixed(2);
+    total_amount.value = price.toFixed(3);
 })
-
 
 
 
